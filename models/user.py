9e11e70a0033a3838e98714a8170c737f51115bc
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-06 18:47:39 +0300

    Add user salt, created_at, image field

--- models/user.py
+++ models/user.py
@@ -1,7 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
+import datetime
 
 
 class User(BaseModel):
-    name: str
-    email: str
+    nickname: str
+    email: EmailStr
     password: str
+    salt: str = ""
+    created_at: datetime.datetime = datetime.datetime.now()
+    image: bytes = Field(..., description="Profile picture as binary data")
+
+
+class UserIn(BaseModel):
+    nickname: str
+    email: EmailStr
+    password: str
+    image: bytes = Field(..., description="Profile picture as binary data")
+
+
+class UserOut(BaseModel):
+    nickname: str
+    email: EmailStr
+    image: bytes = Field(..., description="Profile picture as binary data")

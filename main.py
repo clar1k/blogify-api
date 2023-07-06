e5ff4e2ad2d8140bc31afd9a939a9076f477ce3f
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-06 18:46:32 +0300

    Add python __main__ script

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import uvicorn
 from fastapi import FastAPI
 from routes.user import user
+from routes.posts import posts
 
 app = FastAPI(debug=True)
 app.include_router(user)
+app.include_router(posts)
+
+
+if __name__=="__main__":
+    uvicorn.run('main:app', reload=True)

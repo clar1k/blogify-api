fc4db21ccca428a88bdc65562bdb0bc28bdb2219
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-07 14:11:30 +0300

    Add posts base endpoints

--- routes/posts.py
+++ routes/posts.py
@@ -3,16 +3,18 @@ from fastapi import APIRouter
 posts = APIRouter()
 
 
-@posts.get('/{user_id}')
+@posts.get('/{user_id}', tags=['Posts'])
 def get_post_by_user(user_id: str):
     pass
 
 
-@posts.post('/post/')
+@posts.get('/post/{post_id}', tags=['Posts'])
+def get_post_by_id(post_id: str):
+    pass
+
+
+@posts.post('/post/', tags=['Posts'])
 def create_post():
     pass
 
 
-@posts.get('/post/{post_id}')
-def get_post_by_id(post_id: str):
-    pass

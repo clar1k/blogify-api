29af2df492cf9d9d9acbc71907eb8d788b4b415a
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-07 13:55:29 +0300

    Add auth router

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
-from routes.user import user
-from routes.posts import posts
+from routes import user, posts, auth
 
 app = FastAPI(debug=True)
-app.include_router(user)
-app.include_router(posts)
-
+app.include_router(user.user)
+app.include_router(posts.posts)
+app.include_router(auth.auth)
 
 if __name__=="__main__":
     uvicorn.run('main:app', reload=True)

f29100d47b622d522db11755f2e0b21b921307c9
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-07 14:04:46 +0300

    Change the type of password, salt to the bytes
    Add check_password function for user auth

--- models/user.py
+++ models/user.py
@@ -1,24 +1,27 @@
 from pydantic import BaseModel, Field, EmailStr
+import bcrypt
 import datetime
 
 
 class User(BaseModel):
     nickname: str
     email: EmailStr
-    password: str
-    salt: str = ""
+    password: bytes
+    salt: bytes = b''
+    is_confirm: bool = False
     created_at: datetime.datetime = datetime.datetime.now()
-    image: bytes = Field(..., description="Profile picture as binary data")
 
 
 class UserIn(BaseModel):
     nickname: str
     email: EmailStr
     password: str
-    image: bytes = Field(..., description="Profile picture as binary data")
 
 
 class UserOut(BaseModel):
     nickname: str
     email: EmailStr
-    image: bytes = Field(..., description="Profile picture as binary data")
+
+def check_password(password: str, salt: bytes, hashed_pw: bytes) -> bool:
+    hashed_input = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_input == hashed_pw

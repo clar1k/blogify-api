aff31f5a9bc6975f02427881debd65bd55aa6d5e
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-09 08:13:07 +0300

    Add serialisation methods for post entity's

--- schemas/posts.py
+++ schemas/posts.py
@@ -1,8 +1,10 @@
 def postEntity(post: dict) -> dict:
     return {
+        "title":post['title'],
         "text": post['text'],
         "author_id": post['author_id']
     }
 
-def postsEntity(posts) -> list:
+
+def postsEntity(posts: list) -> list:
     return [postEntity(post) for post in posts]

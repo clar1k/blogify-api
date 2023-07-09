80be4f35c4e95f02c614a61a8b1f2a4444a88cc4
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-09 21:02:28 +0300

    Add loading secret key from config

--- config/mail.py
+++ config/mail.py
@@ -1,39 +1,31 @@
-from os import environ as env
-from dotenv import load_dotenv
 import smtplib
+from config.config import Config
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from itsdangerous import URLSafeSerializer
-s = URLSafeSerializer('secret_key')
 
-load_dotenv('.env')
-class MailConfig():
-    SMTP_SERVER = env.get('MAIL_SERVER')
-    SMTP_PORT = env.get('MAIL_PORT')
-    MAIL_USERNAME = env.get('MAIL_USERNAME')
-    MAIL_PASSWORD = env.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = env.get('MAIL_DEFAULT_SENDER')
+s = URLSafeSerializer(Config.SECRET_KEY)
 
 
-def send_message(recipient: str, token: str) -> bool:
-    with smtplib.SMTP(MailConfig.SMTP_SERVER, MailConfig.SMTP_PORT) as server:
+def send_message(email: str, token: str) -> bool:
+    with smtplib.SMTP(Config.SMTP_SERVER, Config.SMTP_PORT) as server:
         msg = MIMEMultipart()
         
         msg['From'] = "TestieMan4ik"
-        msg['To'] = recipient
+        msg['To'] = email
         msg['Subject'] = "Confirm your account :D"
         
         msg.attach(MIMEText(f"Your link is http://127.0.0.1:8000/confirm/{token}",'plain'))
         
         server.starttls()
-        server.login(MailConfig.MAIL_USERNAME, MailConfig.MAIL_PASSWORD)
+        server.login(Config.MAIL_USERNAME, Config.MAIL_PASSWORD)
         server.send_message(msg)
         return True
 
 
 def generate_confirm_token(email: str):
-    return s.dumps(email, 'secret_key')
+    return s.dumps(email, Config.SECRET_KEY)
 
 
 def get_email_by_token(token):
-    return s.loads(token, 'secret_key')
+    return s.loads(token, Config.SECRET_KEY)

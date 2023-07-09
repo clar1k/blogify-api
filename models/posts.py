08a4cdb66824e2022b97fa7fe6c55ff41246527c
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-09 08:10:20 +0300

    Add fields for the models

--- models/posts.py
+++ models/posts.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from pydantic import BaseModel
-
 class Post(BaseModel):
     author_id: str = ''
+    title: str
     text: str
     likes: int = 0
+    created_at: datetime = datetime.utcnow()
 
 class PostIn(BaseModel):
     token: str
+    title: str
     text: str

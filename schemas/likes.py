a308235e80fc0249cc2bcb3142b128d53c6dd1b9
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-10 20:18:03 +0300

    Add serialise functions for likes

---
+++ schemas/likes.py
@@ -0,0 +1,8 @@
+def likeEntity(like: dict) -> dict:
+    return {
+        "post_id":like['post_id']
+    }
+
+
+def likesEntity(likes: list) -> list:
+    return [likeEntity[like] for like in likes]

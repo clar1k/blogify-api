3b6b02aeba4de89ec349d12782a6103e93f8bfab
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-10 20:10:57 +0300

    Add comments, likes routes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
-from routes import user, posts, auth
+from routes import user, posts, auth, comments, likes
 
 app = FastAPI(debug=True)
 app.include_router(user.user)
 app.include_router(posts.posts)
 app.include_router(auth.auth)
+app.include_router(comments.comments)
+app.include_router(likes.like)
 
 if __name__=="__main__":
     uvicorn.run('main:app', reload=True)

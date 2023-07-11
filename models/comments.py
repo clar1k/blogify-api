f5a846e45450e426d489817d9c0233fda0b57c93
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-11 11:00:11 +0300

    Changed _id to PyObjectId(bson)

--- models/comments.py
+++ models/comments.py
@@ -1,17 +1,14 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
-from bson import ObjectId
+from models.PyObjectId import PyObjectId
 
 class CommentIn(BaseModel):
     content: str
-    post_id: str
+    post_id: PyObjectId
     token: str
 
 class Comment(BaseModel):
     content: str
-    user_id: ObjectId = Field(None) 
-    post_id: str
+    user_id: PyObjectId
+    post_id: PyObjectId
     created_at: datetime = datetime.utcnow()
-    
-    class Config:
-        arbitrary_types_allowed = True

ab5600cbdab2bfc2cd216b79f9beca2ad6ea6f28
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-11 10:59:52 +0300

    Add follows router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from routes import user, posts, auth, comments, likes
+from routes import user, posts, auth, comments, likes, follows
 
 app = FastAPI(debug=True)
 app.include_router(user.user)
@@ -8,6 +8,7 @@ app.include_router(posts.posts)
 app.include_router(auth.auth)
 app.include_router(comments.comments)
 app.include_router(likes.like)
+app.include_router(follows.follows)
 
 if __name__=="__main__":
     uvicorn.run('main:app', reload=True)

157cf189b256f3d2b23c8002744e4bf6362c7ce4
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:57:58 +0300

    Add cleaner code improvements

--- routes/likes.py
+++ routes/likes.py
@@ -3,31 +3,30 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from models.likes import Like
 from bson import ObjectId
-from utils.token import get_id 
+from utils.token import get_id
 from schemas.likes import likesEntity
-from schemas.posts import postEntity,postsEntity
-like = APIRouter(tags=['Likes'])
+from schemas.posts import postEntity, postsEntity
+
+likes = APIRouter(tags=["Likes"])
 
 
-@like.post('/like/post')
+@likes.post("/like/post")
 def like_post(like: Like):
-    if db.likes.find_one({"post_id":like.post_id, "user_id": like.user_id}):
+    if db.likes.find_one({"post_id": like.post_id, "user_id": like.user_id}):
         db.likes.find_one_and_delete(like.dict())
-        db.posts.find_one_and_update( 
-            {"_id":ObjectId(like.post_id)}, 
-            {"$inc": {"likes": -1}}
+        db.posts.find_one_and_update(
+            {"_id": ObjectId(like.post_id)}, {"$inc": {"likes": -1}}
         )
-        return JSONResponse({"message":"Unliked"}, 200)
-    
+        return JSONResponse({"message": "Unliked"}, 200)
+
     db.likes.insert_one(like.dict())
-    db.posts.find_one_and_update( 
-            {"_id":ObjectId(like.post_id)}, 
-            {"$inc": {"likes": 1}}
-        )
-    return JSONResponse({"message":"Liked"}, 201)
+    db.posts.find_one_and_update(
+        {"_id": ObjectId(like.post_id)}, {"$inc": {"likes": 1}}
+    )
+    return JSONResponse({"message": "Liked"}, 201)
 
 
-@like.get('/like/user/posts/{token}')
+@likes.get("/like/user/posts/{token}")
 def get_liked_posts_by_user(token: str) -> JSONResponse:
     uid = get_id(token)
     if likes := db.likes.find({"user_id": ObjectId(uid)}):
@@ -35,7 +34,7 @@ def get_liked_posts_by_user(token: str) -> JSONResponse:
         posts = []
         for like in likes:
             _id = like["post_id"]
-            post = db.posts.find_one({"_id":ObjectId(_id)})
+            post = db.posts.find_one({"_id": ObjectId(_id)})
             posts.append(post)
         return JSONResponse(postsEntity(posts), 200)
-    return JSONResponse({"message":"Invalid token"}, 400)
+    return JSONResponse({"message": "Invalid token"}, 400)

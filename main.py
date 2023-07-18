f14ad359aab7d5ec787e85b9b847a224bf13243c
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:51:38 +0300

    Add a title for docs

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
-from routes import user, posts, auth, comments, likes, follows
+from routes import posts, auth, comments, likes, follows, users
 
-app = FastAPI(debug=True)
-app.include_router(user.user)
-app.include_router(posts.posts)
+app = FastAPI(debug=True, title="Blogify")
+
 app.include_router(auth.auth)
+app.include_router(users.users)
+app.include_router(posts.posts)
 app.include_router(comments.comments)
-app.include_router(likes.like)
+app.include_router(likes.likes)
 app.include_router(follows.follows)
+
 
 if __name__=="__main__":
     uvicorn.run('main:app', reload=True)

ff2f375b5f48dc345a62b43b31e65c40589b7e30
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:53:18 +0300

    Add config and loading from the env file

--- config/config.py
+++ config/config.py
@@ -1,16 +1,16 @@
 from os import environ as env
 from dotenv import load_dotenv
 
-if not load_dotenv('.env'):
-    raise Exception('Could not find dotenv file in root directory')
+if not load_dotenv(".env.development"):
+    raise Exception("Could not find dotenv file")
 
 
 class Config:
-    SECRET_KEY = env.get('SECRET_KEY')
-    SMTP_SERVER = env.get('MAIL_SERVER') 
-    SMTP_PORT = env.get('MAIL_PORT')
-    MAIL_USERNAME = env.get('MAIL_USERNAME')
-    MAIL_PASSWORD = env.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = env.get('MAIL_DEFAULT_SENDER')
-    MONGO_CONNECTION = env.get('MONGO_CONNECTION')
-    UPLOAD_FOLDER = env.get('UPLOAD_FOLDER')
+    SECRET_KEY = env.get("SECRET_KEY")
+    SMTP_SERVER = env.get("MAIL_SERVER")
+    SMTP_PORT = env.get("MAIL_PORT")
+    MAIL_USERNAME = env.get("MAIL_USERNAME")
+    MAIL_PASSWORD = env.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = env.get("MAIL_DEFAULT_SENDER")
+    MONGO_CONNECTION = env.get("MONGO_CONNECTION")
+    UPLOAD_FOLDER = env.get("UPLOAD_FOLDER")

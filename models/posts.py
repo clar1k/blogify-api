961064698798cfad7ba8d30d03e9489a1088441c
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:55:22 +0300

    Add PostUpdate class

--- models/posts.py
+++ models/posts.py
@@ -1,14 +1,22 @@
 from datetime import datetime
 from pydantic import BaseModel
 from models.PyObjectId import PyObjectId
+
+
 class Post(BaseModel):
-    author_id: PyObjectId
+    author_id: PyObjectId = None
     title: str
     text: str
     likes: int = 0
     created_at: datetime = datetime.utcnow()
 
+
 class PostIn(BaseModel):
     token: str
+    title: str
+    text: str
+
+
+class PostUpdate(BaseModel):
     title: str
     text: str

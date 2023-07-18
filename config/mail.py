0dfc039e76d84c99909b26f29fb45def1462f37b
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:54:21 +0300

    Add clean code improvements

--- config/mail.py
+++ config/mail.py
@@ -10,13 +10,15 @@ s = URLSafeSerializer(Config.SECRET_KEY)
 def send_message(email: str, token: str) -> bool:
     with smtplib.SMTP(Config.SMTP_SERVER, Config.SMTP_PORT) as server:
         msg = MIMEMultipart()
-        
-        msg['From'] = "TestieMan4ik"
-        msg['To'] = email
-        msg['Subject'] = "Confirm your account :D"
-        
-        msg.attach(MIMEText(f"Your link is http://127.0.0.1:8000/confirm/{token}",'plain'))
-        
+
+        msg["From"] = "TestieMan4ik"
+        msg["To"] = email
+        msg["Subject"] = "Confirm your account :D"
+
+        msg.attach(
+            MIMEText(f"Your link is http://127.0.0.1:8000/confirm/{token}", "plain")
+        )
+
         server.starttls()
         server.login(Config.MAIL_USERNAME, Config.MAIL_PASSWORD)
         server.send_message(msg)

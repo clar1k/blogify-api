ad9e40810d86ee289b076f2b0e14eb3939ecbdda
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:58:17 +0300

    Add update post endpoint

--- routes/posts.py
+++ routes/posts.py
@@ -1,45 +1,53 @@
-import jwt
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from bson import ObjectId
-from models.posts import PostIn, Post
+from models.posts import PostIn, Post, PostUpdate
 from config.db import db
 from schemas.posts import postEntity, postsEntity
+from utils.token import get_id
 
-posts = APIRouter(tags=['Posts'])
+posts = APIRouter(tags=["Posts"])
 
 
-@posts.get('/post/user/{user_id}')
+@posts.get("/post/user/{user_id}")
 def get_post_by_user(user_id: str) -> JSONResponse:
-    if posts := db.posts.find({"author_id":user_id}):
+    if posts := db.posts.find({"author_id": user_id}):
         return JSONResponse(postsEntity(posts), 200)
-    return JSONResponse({"message":"Posts not found"},400)
+    return JSONResponse({"message": "Posts not found"}, 400)
 
 
-@posts.get('/post/{post_id}')
+@posts.get("/post/{post_id}")
 def get_post_by_id(post_id: str):
-    if post := db.posts.find_one({"_id":ObjectId(post_id)}):
+    if post := db.posts.find_one({"_id": ObjectId(post_id)}):
         return JSONResponse(postEntity(post), 200)
-    return JSONResponse({"message":"There`s no post by this id"}, 400)
+    return JSONResponse({"message": "There`s no post by this id"}, 400)
 
 
-@posts.post('/post/')
+@posts.post("/post/")
 def create_post(post: PostIn) -> JSONResponse:
-    token_decode = jwt.decode(post.token.encode('utf-8'),'secret_key', algorithms=['HS256'])
-    _id = token_decode['_id']
+    _id = get_id(post.token)
     new_post: Post = Post.parse_obj(post)
-    new_post.author_id = str(_id)
+    new_post.author_id = _id
     db.posts.insert_one(new_post.dict())
-    return JSONResponse({"message":"Post succesfulyy created"}, 201)
+    return JSONResponse({"message": "Post succesfulyy created"}, 201)
 
 
-@posts.delete('/post/{post_id}')
-def delete_post_by_id(post_id: str) -> JSONResponse:
-    if db.posts.find_one_and_delete({"_id":ObjectId(post_id)}):
-        return JSONResponse({"message":"Post was deleted"}, 200)
-    return JSONResponse({"message":"Post not found"}, 400)
+@posts.delete("/post/")
+async def delete_post_by_title_and_text(post: PostIn) -> JSONResponse:
+    _id = get_id(post.token)
+    post_filter = {"title": post.title, "text": post.text, "author_id": _id}
+    is_post_deleted = db.posts.find_one_and_delete(post_filter)
+    if is_post_deleted:
+        return JSONResponse({"message": "Post deleted"}, 200)
+    
+    return JSONResponse({"message": "Post not found"}, 400)
 
 
-@posts.put('/post')
-def update_post() -> JSONResponse:
-    return
+@posts.put("/post/")
+def update_post(post: PostIn, post_update: PostUpdate) -> JSONResponse:
+    _id = get_id(post.token)
+    _filter = {"title": post.title, "text": post.text, "author_id": _id}
+    update = {"$set": {"title": post_update.title, "text": post_update.text}}
+    if db.posts.find_one_and_update(_filter, update):
+        return JSONResponse({"message": "Post updated"}, 200)
+    return JSONResponse({"message": "Post not found"}, 400)

b7925dfa5d1b0188cae4ee5f725de2db90812155
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-19 01:54:34 +0300

    Add comment body pydantic model

--- models/comments.py
+++ models/comments.py
@@ -2,13 +2,19 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from models.PyObjectId import PyObjectId
 
-class CommentIn(BaseModel):
-    content: str
-    post_id: PyObjectId
-    token: str
 
 class Comment(BaseModel):
     content: str
-    user_id: PyObjectId
-    post_id: PyObjectId
+    user_id: PyObjectId = None
+    post_id: PyObjectId = None
     created_at: datetime = datetime.utcnow()
+
+
+class CommentIn(BaseModel):
+    token: str
+    content: str
+    post_id: PyObjectId
+
+
+class CommentUpdate(BaseModel):
+    content: str

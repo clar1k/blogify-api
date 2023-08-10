4763be21ea4432a5c40ca69930160e35279ad45e
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-08-10 18:05:05 +0300

    Add graphql router to all routers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from routes import posts, auth, comments, likes, follows, users
+from gql import graphql_app
 
 app = FastAPI(debug=True, title="Blogify")
 
@@ -10,7 +11,8 @@ app.include_router(posts.posts)
 app.include_router(comments.comments)
 app.include_router(likes.likes)
 app.include_router(follows.follows)
+app.include_router(graphql_app, prefix="/graphql")
 
 
-if __name__=="__main__":
-    uvicorn.run('main:app', reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)

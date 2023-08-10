7ad8f982ef4c5308968016b72f020ab2e3d70114
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-08-10 18:07:54 +0300

    Add clean code improvements

--- utils/token.py
+++ utils/token.py
@@ -6,14 +6,14 @@ from config.db import db
 
 
 def get_id(token: str) -> ObjectId:
-    token = jwt.decode(token,Config.SECRET_KEY, ['HS256'])
-    _id = token['_id']
+    token = jwt.decode(token, Config.SECRET_KEY, ["HS256"])
+    _id = token["_id"]
     _id = ObjectId(_id)
     return _id
 
 
 def is_token_expired(decoded_token: dict) -> bool:
-    expiration_time = decoded_token['exp']
+    expiration_time = decoded_token["exp"]
     expiration_time = datetime.utcfromtimestamp(expiration_time)
     time_now = datetime.utcnow()
     if expiration_time < time_now:
